5e04a908b64146462495a39ab2e9b04dfdc03816
onushukvyatcheslav <onushukvyatcheslav@gmail.com>
2022-09-25 08:39:13 +0300

    f

---
+++ Game.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pair_CSharp_28._08._2022
+{
+    public class Game
+    {
+        public string? Name { set; get; }
+        public string? RealeseDate { set; get; }
+        public double? Price { set; get; }
+        public string? TypeOf { set; get; }
+
+        public Game()
+        {
+            Name = "";
+            RealeseDate = "";
+            Price = 0;
+            TypeOf = "";
+        }
+        public Game(string Name, string RealeseDate, double Price, string TypeOf)
+        {
+            this.Name = Name;
+            this.RealeseDate = RealeseDate;
+            this.Price = Price;
+            this.TypeOf = TypeOf;
+        }
+
+        public void Start()
+        {
+            Console.WriteLine("Start Game!");
+        }
+        public void End()
+        {
+            Console.WriteLine("End Game!");
+        }
+
+    }
+}
---
+++ IGame.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pair_CSharp_28._08._2022
+{
+    public interface IGame
+    {
+
+        public void Start();
+        public void End();
+    }
+}
